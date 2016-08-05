bf0222efd8cc8e2ac90bce6de235b7bd13ade806
Alex <darthsanches@gmail.com>
2016-08-05 04:29:58 +0300

    add restore state and some change button disconect

--- app/src/main/java/com/darthsanches/mappointsmaker/ui/MapFragment.java
+++ app/src/main/java/com/darthsanches/mappointsmaker/ui/MapFragment.java
@@ -3,12 +3,14 @@ package com.darthsanches.mappointsmaker.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.darthsanches.mappointsmaker.App;
 import com.darthsanches.mappointsmaker.R;
@@ -25,6 +27,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
 
+import java.util.ArrayList;
+
 import javax.inject.Inject;
 
 /**
@@ -32,6 +36,7 @@ import javax.inject.Inject;
  */
 public class MapFragment extends SupportMapFragment implements OnMapReadyCallback {
 
+    private static final String POINTS = "points";
     private static float ZOOM_LEVEL = 10.0f;
 
     @Inject
@@ -39,6 +44,7 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
 
     private GoogleMap map;
     private LatLng lastLocation;
+    private ArrayList<LatLng> points = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle bundle) {
@@ -60,12 +66,19 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
         Button disconnectButton = new Button(getContext());
         disconnectButton.setText(R.string.quit_text);
         disconnectButton.setTextSize(getResources().getDimension(R.dimen.button_text_font_size));
-        disconnectButton.setLayoutParams(new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.BOTTOM));
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.BOTTOM);
+        int padding = (int)(getResources().getDimension(R.dimen.base_margin)
+                * ((float)getContext().getResources().getDisplayMetrics().densityDpi
+                / DisplayMetrics.DENSITY_DEFAULT));
+        lp.setMargins(padding, padding, padding, padding);
+        disconnectButton.setLayoutParams(lp);
         disconnectButton.bringToFront();
-        int padding = (int) getResources().getDimension(R.dimen.base_margin);
-        disconnectButton.setPadding(padding, padding, padding, padding);
         ((FrameLayout) view).addView(disconnectButton);
         disconnectButton.setOnClickListener(v -> onDisconnectClick());
+
+        if (savedInstanceState != null) {
+            points = savedInstanceState.getParcelableArrayList(POINTS);
+        }
     }
 
     @Override
@@ -84,10 +97,12 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
     public void newPointsComming(PointsCommingEvent event) {
         if (map == null) return;
         map.clear();
+        points.clear();
         for (Point point : event.getPoints()) {
-            map.addMarker(new MarkerOptions().position(new LatLng(point.lat, point.lon)));
+            LatLng latlng = new LatLng(point.lat, point.lon);
+            points.add(latlng);
+            map.addMarker(new MarkerOptions().position(latlng));
         }
-
     }
 
     @Subscribe
@@ -102,6 +117,9 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
         moveCamera();
+        for (LatLng point : points) {
+            map.addMarker(new MarkerOptions().position(point));
+        }
     }
 
     private void moveCamera() {
@@ -113,5 +131,11 @@ public class MapFragment extends SupportMapFragment implements OnMapReadyCallbac
     public void onDisconnectClick() {
         getActivity().stopService(new Intent(getActivity(), SocketService.class));
         ((MainActivity) getActivity()).openLoginFragment();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle bundle) {
+        bundle.putParcelableArrayList(POINTS, points);
+        super.onSaveInstanceState(bundle);
     }
 }
